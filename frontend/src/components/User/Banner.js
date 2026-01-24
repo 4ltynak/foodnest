1eb8bb4a63e854463b5e60e9d3a67eb34f7c69b5
4ltynak <kineticjacks@gmail.com>
2026-01-24 08:27:13 +0800

    Update Banner.js

--- frontend/src/components/User/Banner.js
+++ frontend/src/components/User/Banner.js
@@ -9,10 +9,10 @@ function Banner(){
         <Grid container direction="column" p={2}  alignContent={"center"} justifyContent="center" textAlign={"center"} className="banner-section" 
         sx={{width: "100%", backgroundImage: "url(/img/banner-img.jpg)", backgroundSize: "cover", alignItems: "center"}}>
             <Grid size={12}>
-                <Typography variant="h1" sx={{fontSize: {xs: "2.5rem"}}} alignSelf={"center"}>FOODNEST</Typography>
+                <Typography variant="h1" sx={{fontSize: {xs: "2.5rem", md: "inherit"}}} alignSelf={"center"}>FOODNEST</Typography>
             </Grid>
             <Grid size={12}>
-                <Typography variant="h2" color="white" sx={{fontSize: {xs: "1.5rem"}}}>Where great food meets great places.</Typography>
+                <Typography variant="h2" color="white" sx={{fontSize: {xs: "1.5rem", md: "inherit"}}}>Where great food meets great places.</Typography>
             <Grid size={12}>
                 <Button variant="contained" color="white" href="#meals">DISCOVER</Button>
             </Grid>            
